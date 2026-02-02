313ac6d5824dfc684fbde924387bbc42cea03763
Dorje Pradhan <dorjekmp@gmail.com>
2026-02-02 05:12:15 +0000

    enviroment example in python

---
+++ dorjepradhan.com/public_html/hw2/python/enviroment-python.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+
+print("Cache-Control: no-cache")
+print("Content-Type: text/html\n")
+
+print("<!DOCTYPE html><html><head><title>Environment Variables</title></head><body>")
+print("<h1 align='center'>Environment Variables</h1><hr>")
+
+for k in sorted(os.environ.keys()):
+    v = os.environ.get(k, "")
+    print(f"<b>{k}:</b> {v}<br />")
+
+print("</body></html>")
